30bea5c46c0e1aa30e528d654a12c897f6076534
Krzysztof Jankowski <w84death@gmail.com>
2014-08-21 20:53:54 +0200

    engine ready

--- game2.js
+++ game2.js
@@ -40,7 +40,7 @@ window.requestAnimFrame = (function(){
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 var Gfx = function(){};
-Gfx.prototype.init = function(){
+Gfx.prototype.init = function(params){
     this.loaded = 0;
     this.sprites = {
         logo: new Image(),
@@ -59,6 +59,20 @@ Gfx.prototype.init = function(){
     };
     this.sprites.tileset.onload = function(){
         game.gfx.loaded++;
+        game.gfx.init_tileset();
+    };
+
+    for (var i = 0; i < params.layers; i++) {
+        var canvas = document.createElement('canvas');
+        canvas.width = game.screen.width;
+        canvas.height = game.screen.height;
+        var ctx = canvas.getContext("2d");
+        game.layers.push({
+            canvas: canvas,
+            ctx: ctx,
+            render: false
+        });
+        document.getElementById('game').appendChild(canvas);
     };
 };
 Gfx.prototype.load = function(){
@@ -71,6 +85,41 @@ Gfx.prototype.load = function(){
     }
     return false;
 };
+Gfx.prototype.clear = function(layer){
+    game.layers[layer].ctx.clearRect(
+        0, 0,
+        game.screen.width, game.screen.height
+    );
+};
+Gfx.prototype.init_tileset = function(){
+    var canvas = document.createElement('canvas');
+    canvas.width = this.sprites.tileset.width;
+    canvas.height = this.sprites.tileset.height;
+    var ctx = canvas.getContext("2d");
+
+    ctx.drawImage(this.sprites.tileset,0,0);
+
+    this.tileset = [];
+    for (var x = 0; x < canvas.width/game.world.sprite_size; x++) {
+        for (var y = 0; y < canvas.height/game.world.sprite_size; y++) {
+            this.tileset.push(
+                ctx.getImageData(
+                    game.world.sprite_size * x,
+                    game.world.sprite_size * y,
+                    game.world.sprite_size,
+                    game.world.sprite_size
+                )
+            );
+        }
+    }
+};
+Gfx.prototype.put_tile = function(params){
+    game.layers[params.layer].ctx.putImageData(
+        this.tileset[params.id],
+        params.x*game.world.sprite_size,
+        params.y*game.world.sprite_size
+    );
+};
 
 /*
 *
@@ -79,64 +128,87 @@ Gfx.prototype.load = function(){
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 var Gui = function(){};
+Gui.prototype.init = function(params){
+    this.layer = params.layer;
+};
+Gui.prototype.clear = function(){
+    game.layers[this.layer].ctx.clearRect(
+        0, 0,
+        game.screen.width, game.screen.height
+    );
+};
 Gui.prototype.draw_logo = function(params){
-    game.ctx.textBaseline = 'bottom';
-    game.ctx.textAlign = 'center';
+    var ctx = game.layers[this.layer].ctx;
 
+    ctx.textBaseline = 'bottom';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = '#fff';
+    ctx.font = 'bold 10px sans-serif';
+    ctx.strokeStyle = '#fff';
 
-    game.ctx.fillStyle = '#fff';
-    game.ctx.font = 'bold 0.7em sans-serif';
-    game.ctx.fillText('P1X PRESENTS',
+    ctx.fillText('P1X PRESENTS',
         game.screen.width*0.5 << 0,
         (game.screen.height*0.5 << 0) - 36
     );
 
-    game.ctx.beginPath();
-    game.ctx.moveTo(24,(game.screen.height*0.5 << 0) - 32);
-    game.ctx.lineTo(game.screen.width-24,(game.screen.height*0.5 << 0) - 32);
-    game.ctx.strokeStyle = '#2F484E';
-    game.ctx.stroke();
-
-    /*game.ctx.fillStyle = '#eb8931';
-    game.ctx.font = 'bold 1em sans-serif';
-    game.ctx.fillText('Tribes Life',
-        game.screen.width*0.5 << 0,
-        game.screen.height*0.5 << 0
-    );*/
-
-    game.ctx.drawImage(game.gfx.sprites.logo,
+    ctx.drawImage(game.gfx.sprites.logo,
         (game.screen.width*0.5 << 0)-96,
-        (game.screen.height*0.5 << 0)-32
+        (game.screen.height*0.5 << 0)-42
     );
 
-    game.ctx.beginPath();
-    game.ctx.moveTo(24,(game.screen.height*0.5 << 0) + 32);
-    game.ctx.lineTo(game.screen.width-24,(game.screen.height*0.5 << 0) + 32);
-    game.ctx.strokeStyle = '#2F484E';
-    game.ctx.stroke();
+    game.layers[this.layer].ctx.beginPath();
+    game.layers[this.layer].ctx.moveTo(24,(game.screen.height*0.5 << 0) + 32);
+    game.layers[this.layer].ctx.lineTo(game.screen.width-24,(game.screen.height*0.5 << 0) + 32);
+    game.layers[this.layer].ctx.stroke();
+
+    game.layers[this.layer].ctx.fillText('JS13KGAME COMPO',
+        game.screen.width*0.5 << 0,
+        (game.screen.height*0.5 << 0)+54
+    );
 
     if(game.timer % 2 == 1){
-        game.ctx.fillStyle = '#fff';
-        game.ctx.font = 'bold 0.7em sans-serif';
-        game.ctx.fillText('CLICK TO START',
+        game.layers[this.layer].ctx.fillText('CLICK TO START',
             game.screen.width*0.5 << 0,
-            (game.screen.height*0.5 << 0) + 48);
+            (game.screen.height*0.5 << 0) + 70);
     }
+
+    ctx.beginPath();
+    ctx.moveTo(24,(game.screen.height*0.5 << 0) + 80);
+    ctx.lineTo(game.screen.width-24,(game.screen.height*0.5 << 0) + 80);
+    ctx.stroke();
+
+
 };
 Gui.prototype.draw_fps = function(){
-    game.ctx.fillStyle = '#fff';
-    game.ctx.font = 'bold 0.5em sans-serif';
-    game.ctx.textBaseline = 'bottom';
-    game.ctx.textAlign = 'left';
-    game.ctx.fillText('FPS:'+game.fps, 6, 12);
+    var ctx = game.layers[this.layer].ctx;
+    ctx.fillStyle = '#fff';
+    ctx.font = 'bold 0.5em sans-serif';
+    ctx.textBaseline = 'bottom';
+    ctx.textAlign = 'left';
+    ctx.fillText('FPS:'+game.fps, 6, 12);
+};
+Gui.prototype.draw_pointer = function(){
+    var x = (game.pointer.pos.x / game.screen.scale) << 0,
+        y = (game.pointer.pos.y / game.screen.scale) << 0;
+
+    game.layers[this.layer].ctx.drawImage(game.gfx.sprites.pointer,
+        x,
+        y
+    );
 };
 
+/*
+*
+*   input function
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 var Input = function(){};
-Input.prototype.init = function(canvas){
-    canvas.addEventListener('mousedown', this.enable_pointer, false);
-    canvas.addEventListener('mouseup', this.disable_pointer, false);
-    canvas.addEventListener('mousemove', this.track_pointer, false);
-    canvas.addEventListener("contextmenu", function(e){
+Input.prototype.init = function(){
+    document.body.addEventListener('mousedown', this.enable_pointer, false);
+    document.body.addEventListener('mouseup', this.disable_pointer, false);
+    document.body.addEventListener('mousemove', this.track_pointer, false);
+    document.body.addEventListener("contextmenu", function(e){
         e.preventDefault();
     }, false);
 };
@@ -187,15 +259,16 @@ var game = {
     input: new Input(),
 
     fps: 0,
+    layers: [],
     canvas: null,
     ctx: null,
     screen: {
         width: null,
-        height: null
+        height: null,
+        scale: 4
     },
     world: {
         sprite_size: 8,
-        scale: 4,
         width: null,
         height: null
     },
@@ -215,25 +288,32 @@ var game = {
 
     init: function(){
         // canvas sizes
-        this.screen.width = ((window.innerWidth/this.world.sprite_size)*this.world.scale)<<0;
-        this.screen.height = ((window.innerHeight/this.world.sprite_size)*this.world.scale)<<0;
-        this.canvas = document.getElementById('canvas');
-        this.canvas.width = this.screen.width;
-        this.canvas.height = this.screen.height;
-        this.ctx = this.canvas.getContext('2d');
+        this.screen.width = (window.innerWidth/this.screen.scale)<<0;
+        this.screen.height = (window.innerHeight/this.screen.scale)<<0;
+        //this.canvas = document.getElementById('canvas');
+        //this.canvas.width = this.screen.width;
+        //this.canvas.height = this.screen.height;
+        //this.ctx = this.canvas.getContext('2d');
 
         // game world size (for now as big as screen)
         this.world.width = (this.screen.width/this.world.sprite_size)<<0;
         this.world.height = (this.screen.height/this.world.sprite_size)<<0;
 
-        // mouse events
-        this.input.init(this.canvas);
-
         // init game timer
         window.setInterval(game.inc_timer,500);
 
         // graphics init
-        this.gfx.init();
+        this.gfx.init({
+            layers: 3
+        });
+
+        // gui init
+        this.gui.init({
+            layer: 2
+        })
+
+        // mouse events
+        this.input.init();
     },
 
     /*
@@ -255,6 +335,7 @@ var game = {
     new_game: function(){
         this.pointer.enable = false;
         this.generate_island();
+        this.layers[0].render = true;
         this.timer = 0;
     },
 
@@ -292,19 +373,48 @@ var game = {
     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
     render: function(delta_time){
-        this.ctx.clearRect(0, 0, this.screen.width, this.screen.height);
+        //this.ctx.clearRect(0, 0, this.screen.width, this.screen.height);
+        this.gui.clear();
 
         switch(this.state){
             case 'intro':
+
                 this.gui.draw_logo();
             break;
             case 'game':
+                // render background terrain
+                if(this.layers[0].render){
+                    for (var x = 0; x < this.world.width; x++) {
+                        for (var y = 0; y < this.world.height; y++) {
+                            this.gfx.put_tile({
+                                id:1,
+                                x:x,
+                                y:y,
+                                layer: 0
+                            });
+                        }
+                    }
+                    this.layers[0].render = false;
+                }
+                // render entities
+                if(this.layers[1].render){
+                    for (var x = 0; x < this.world.width; x++) {
+                        for (var y = 0; y < this.world.height; y++) {
+
+                        }
+                    }
+                    this.layers[1].render = false;
+                }
+                // render gui
                 this.gui.draw_fps();
             break;
             case 'game_over':
                 this.gui.draw_game_over();
             break;
         }
+
+        this.gui.draw_pointer();
+
 
     },
 
